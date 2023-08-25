d68d85de26726c3857d21f07ff187ad409048ec3
mmindykowski <128606230+mmindykowski@users.noreply.github.com>
2023-08-25 22:02:35 +0200

    zmiana nazwy folderu

--- delete-move/index.js
+++ delete-move/index.js
@@ -1,2 +1,14 @@
 const fs = require("fs");
 const path = require("path");
+
+fs.rename(
+  path.join(__dirname, "data"),
+  path.join(__dirname, "data-temp"),
+  function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Zmieniono lub przeniesiono");
+    }
+  }
+);
