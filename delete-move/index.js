588d73ead787311f19e1b10eb402a56e01c029d8
mmindykowski <128606230+mmindykowski@users.noreply.github.com>
2023-08-25 22:20:34 +0200

    stworzenie folderu i zmiana plus przesniesienie

--- delete-move/index.js
+++ delete-move/index.js
@@ -1,9 +1,25 @@
 const fs = require("fs");
 const path = require("path");
 
+function createDir(path) {
+  fs.mkdir(path, function (err) {
+    if (err) {
+      if (err.code === "EEXIST") {
+        console.log("Folder już istnieje");
+        return;
+      }
+      console.log(err);
+    } else {
+      console.log("Stworzono folder");
+    }
+  });
+}
+
+createDir(path.join(__dirname, "data-temp"));
+
 fs.rename(
-  path.join(__dirname, "data"),
-  path.join(__dirname, "data-temp"),
+  path.join(__dirname, "data", "students-first-year.txt"),
+  path.join(__dirname, "data-temp", "temp-students-first-year.txt"),
   function (err) {
     if (err) {
       console.log(err);
